a94ea215c58587ca9d8d60b85306de5acaa3e98b
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2024-01-31 08:30:53 +0000

    update bubbleSort

--- sorting/bubbleSort.cpp
+++ sorting/bubbleSort.cpp
@@ -1,17 +1,20 @@
+
 #include<bits/stdc++.h>
 
 using namespace std;
 
 void bubbleSort(int arr[],int n){
-    for(int i=0;i<n;i++){
+    for(int i=0;i<n-1;i++){
         for(int j=0;j<n-i;j++){
             if(arr[j]>arr[j+1])
             swap(arr[j],arr[j+1]);
         }
     }
+
 }
 
-int main(){
+int main()
+{
     int n;
     cin>>n;
     int arr[n];
@@ -22,5 +25,6 @@ int main(){
     for(int i=0;i<n;i++){
         cout<<arr[i]<<" ";
     }
+
     return 0;
 }
