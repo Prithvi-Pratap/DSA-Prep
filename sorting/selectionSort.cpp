7aa5b04bd18c9f97efb31cd4885f42b56ecc5308
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2024-02-03 11:02:02 +0000

    UPDATE

--- sorting/selectionSort.cpp
+++ sorting/selectionSort.cpp
@@ -21,3 +21,35 @@ int main(){
     }
     return 0;
 }
+
+
+//visualization
+
+// #include<bits/stdc++.h>
+
+// using namespace std;
+
+// int main(){
+//     int n,min;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0; i<n; i++){
+//         cin>>arr[i];
+//     }
+//     for(int i=0;i<n;i++){
+//      for(int j=i;j<n;j++){
+//         if(arr[j]<arr[i]){
+//             swap(arr[j],arr[i]);
+//             cout<<"step"<<j<<":";
+//             for(int k=0;k<n;k++){
+//                 cout<<arr[k]<<" ";
+//             }
+//             cout<<endl;
+//         }
+//      }   
+//     }
+//     // for(int i=0;i<n;i++){
+//     //     cout<<arr[i]<<" ";
+//     // }
+//     return 0;
+// }
