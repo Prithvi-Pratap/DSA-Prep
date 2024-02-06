b40806c44dbdbe9774f9296153c0bfabbbc4bf28
Code Pharaoh <99997608+Prithvi-Pratap@users.noreply.github.com>
2024-02-06 08:33:34 +0000

    update

---
+++ array/checkIfSorted.cpp
@@ -0,0 +1,57 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+void rotate(vector<int> &arr, int n)
+{
+    for (int i = 0; i < n; i++)
+    {
+        swap(arr[n - i], arr[i]);
+    }
+}
+
+int checkSorted(vector<int> arr, int n)
+{
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[i] >= arr[i - 1])
+        {
+        }
+        else
+        {
+            return 0;
+        }
+    }
+    return true;
+}
+bool check(vector<int> &arr)
+{
+    int n = arr.size();
+    int length =n;
+    int sorted = 0;
+    while (length)
+    {
+        if (checkSorted(arr, n))
+        {
+            return true;
+        }
+        length--;
+    }
+    rotate(arr, n);
+    return false;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int> arr;
+    for (int i = 0; i < n; i++)
+    {
+        int element;
+        cin >> element;
+        arr.push_back(element);
+    }
+    cout << check(arr);
+
+    return 0;
+}
