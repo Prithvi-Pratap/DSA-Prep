c32c0c276e17a965ddf639dabdc9b982117b5e67
Prithvi-Pratap <pratapprithvi768@gmail.com>
2024-02-08 02:28:45 +0530

    update

---
+++ array/rotateArrayByK.cpp
@@ -0,0 +1,57 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// vector<int> rotateArray(vector<int>arr, int n,int k) {
+//     // Write your code here.
+//     int temp[k];
+//     // int n=arr.size();
+//     for(int i=0;i<k;i++){
+//         temp[i]=arr[i];
+//     }
+//     for(int i=k;i<n;i++){
+//         arr[i-k]=arr[i];
+//     }
+//     int j=0;
+//     for(int i=k+1;i<n;i++){
+//         arr[i]=temp[j];
+//         j++;
+//     }
+//      return arr;
+// }
+
+vector<int> rotateArray(vector<int> arr, int n, int k) {
+    int temp[k];
+    for (int i = 0; i < k; i++) {
+        temp[i] = arr[i];
+    }
+    for (int i = k; i < n; i++) {
+        arr[i - k] = arr[i];
+    }
+    for (int i = n - k, j = 0; i < n; i++, j++) {
+        arr[i] = temp[j];
+    }
+    return arr;
+}
+
+
+
+int main(){
+    int n;
+    cin>>n;
+    vector<int> arr;
+    for(int i=0;i<n;i++){
+        int element;
+        cin>>element;
+        arr.push_back(element);
+    }
+    int k;
+    cin>>k;
+
+    arr=rotateArray(arr,n,k);
+    for(auto elem:arr){
+        cout<<elem<<" ";
+    }
+    
+    return 0;
+
+}
