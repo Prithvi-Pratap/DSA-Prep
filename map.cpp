cc44a2e72638ba4d0de355323d92d89ae48ff209
Prithvi-Pratap <pratapprithvi768@gmail.com>
2024-02-18 23:46:20 +0530

    map

--- map.cpp
+++ map.cpp
@@ -1,6 +1,40 @@
 //map stores the values in a sorted format, non-decreasing order.
+//it is a associative container , it stores unique key-value pairs,where each key is unique to a corresponding value
+
+
+// #include<bits/stdc++.h>
+// using namespace std;
+
+// int main(){
+//     int n;
+//     cin>>n;
+//     int arr[n];
+//     for(int i=0;i<n;i++){
+//         cin>>arr[i];
+//     }
+
+//     //pre-calculation ,updating the map
+//     map<int,int> mpp;
+//     for(int i=0;i<n;i++){
+//         mpp[arr[i]]++;
+//     }
+
+//     //fetching
+//     int queries;
+//     cin>>queries;
+//     while(queries--){
+//         int num;
+//         cin>>num;
+//         cout<<mpp[num]<<endl;
+//     }
+
+//     return 0;
+// }
+
+
 #include<bits/stdc++.h>
 using namespace std;
+
 
 int main(){
     int n;
@@ -10,20 +44,37 @@ int main(){
         cin>>arr[i];
     }
 
-    //pre-calculation ,updating the map
-    map<int,int> mpp;
+    //pre-compute
+    map<int,int> myMap;
     for(int i=0;i<n;i++){
-        mpp[arr[i]]++;
-    }
+        myMap[arr[i]]++;
+    }  
 
     //fetching
     int queries;
     cin>>queries;
     while(queries--){
-        int num;
-        cin>>num;
-        cout<<mpp[num]<<endl;
+        int n;
+        cin>>n;
+        cout<<myMap[n]<<endl;
     }
-
-    return 0;
+return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
